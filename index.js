cd6aa8d9f571810517842afcd9dd825952885355
TAKEO Ken <s1320229@linsv211.u-aizu.ac.jp>
2025-06-27 11:38:19 +0900

    Greetings added

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
 function main(){
-	console.log("Rolling dice...")
-	const die1 = Math.floor(Math.random() * 6 + 1);
-	const die2 = Math.floor(Math.random() * 6 + 1)
-	console.log("Die 1: " + die1)
-	console.log("Die 2: " + die2)
-	console.log("Total value: " + (Number(die1)+Number(die2)))
+	rl.question('What is your name?\n>',(name) => {
+		console.log(`Hello, ${name}!`)
+		console.log("Rolling dice...")
+		const die1 = Math.floor(Math.random() * 6 + 1);
+		const die2 = Math.floor(Math.random() * 6 + 1)
+		console.log("Die 1: " + die1)
+		console.log("Die 2: " + die2)
+		console.log("Total value: " + (Number(die1)+Number(die2)))
+		rl.close();
+	});
 }
 
 main()
